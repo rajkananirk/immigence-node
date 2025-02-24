ae9d28488c8b6c76238e4da8aeb2e3d272f8ec17
rajkananirk <rajkanani51@gmail.com>
2025-02-24 22:08:45 +0530

    schema update

--- app/schema/schema.model.js
+++ app/schema/schema.model.js
@@ -7,6 +7,7 @@ const adminSchema = new mongoose.Schema({
     password: { type: String, required: true }
 }, { timestamps: true });
 
+
 const expressEntryDrawSchema = new mongoose.Schema({
     title: { type: String, required: true },
     types: { type: Object, required: true },
